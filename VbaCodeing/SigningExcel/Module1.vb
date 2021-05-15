1158b218086746764fd146c5c7a0f4d4384059bc
tobiasImfeld <tobias_imfeld@hotmail.com>
2021-05-15 15:53:48 +0200

    Try to add signature

--- VbaCodeing/SigningExcel/Module1.vb
+++ VbaCodeing/SigningExcel/Module1.vb
@@ -6,19 +6,23 @@
     End Sub
 
     Sub SetSignature()
-        Dim objWB, objExcel, message
+        Dim objWB, objExcel, activeWB
 
         objExcel = CreateObject("Excel.Application")
-        objWB = objExcel.WorkBooks.Open("C:\Temp\Test1.xlsx")
+        objWB = objExcel.Application.Workbooks.Open("C:\Temp\Test1.xlsx")
         If objWB.Equals(vbNull) Then
-            MsgBox("objWB is null: ")
+            MsgBox("Could not obpen Excel file")
         Else
-            objExcel.ActiveWorkbook.Signatures.AddNonVisibleSignature("4157e14126de03994c7e41c6d36d8ea7")
+            'objExcel.ActiveWorkbook.Signatures.AddNonVisibleSignature("4157e14126de03994c7e41c6d36d8ea7")
+            'objExcel.ActiveWorkbook.Signatures.AddSignatureLine 'Works for manually set signature.
+            activeWB = objExcel.ActiveWorkbook
+
+            MsgBox("Excel has signature: " & activeWB.Signatures.AddNonVisibleSignature("4157e14126de03994c7e41c6d36d8ea7"))
+
             objWB.close
             objExcel.Quit
         End If
 
     End Sub
-
 
 End Module
