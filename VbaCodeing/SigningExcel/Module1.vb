68936136ac3a0486bf886b017202bc2f24ff11fb
tobiasImfeld <tobias_imfeld@hotmail.com>
2021-05-15 14:33:53 +0200

    Introduce project SigningExcel

---
+++ VbaCodeing/SigningExcel/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        SetSignature()
+
+    End Sub
+
+    Sub SetSignature()
+        Dim objWB, objExcel, message
+
+        objExcel = CreateObject("Excel.Application")
+        objWB = objExcel.WorkBooks.Open("C:\Temp\Test1.xlsx")
+        If objWB.Equals(vbNull) Then
+            MsgBox("objWB is null: ")
+        Else
+            objExcel.ActiveWorkbook.Signatures.AddNonVisibleSignature("4157e14126de03994c7e41c6d36d8ea7")
+            objWB.close
+            objExcel.Quit
+        End If
+
+    End Sub
+
+
+End Module
